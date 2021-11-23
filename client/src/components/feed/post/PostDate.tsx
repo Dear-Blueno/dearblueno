e45c53104546d5924b7432ca220e62872e85bcc5
Dylan Hu <dylanhu7@gmail.com>
2021-11-23 06:46:24 -0500

    fix date formatting for singular unit

--- client/src/components/feed/post/PostDate.tsx
+++ client/src/components/feed/post/PostDate.tsx
@@ -9,16 +9,28 @@ function PostDate(props: PostDateProps) {
   const formatDuration = (duration: string) => {
     if (duration.includes(" seconds")) {
       return duration.replace(" seconds", "s");
+    } else if (duration.includes(" second")) {
+      return duration.replace(" second", "s");
     } else if (duration.includes(" minutes")) {
       return duration.replace(" minutes", "m");
+    } else if (duration.includes(" minute")) {
+      return duration.replace(" minute", "m");
     } else if (duration.includes(" hours")) {
       return duration.replace(" hours", "h");
+    } else if (duration.includes(" hour")) {
+      return duration.replace(" hour", "h");
     } else if (duration.includes(" days")) {
       return duration.replace(" days", "d");
+    } else if (duration.includes(" day")) {
+      return duration.replace(" day", "d");
     } else if (duration.includes(" months")) {
       return duration.replace(" months", "mo");
+    } else if (duration.includes(" month")) {
+      return duration.replace(" month", "mo");
     } else if (duration.includes(" years")) {
       return duration.replace(" years", "y");
+    } else if (duration.includes(" year")) {
+      return duration.replace(" year", "y");
     } else {
       return duration;
     }
