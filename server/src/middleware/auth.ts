070d86970e0523a8ff884b0395b4020216daf01e
Nicholas Bottone <nick@bottone.io>
2021-12-21 23:46:12 -0500

    Modify middleware to support testing

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -3,6 +3,11 @@ import { IUser } from "src/models/User";
 
 // check if user is authenticated
 export const authCheck = (req: Request, res: Response, next: NextFunction) => {
+  // If in testing mode, get user from req.body
+  if (process.env.NODE_ENV === "test") {
+    req.user = req.body.user;
+  }
+
   if (!req.user) {
     res.status(401).send("You are not logged in");
     return;
@@ -12,6 +17,11 @@ export const authCheck = (req: Request, res: Response, next: NextFunction) => {
 
 // check if user is a moderator
 export const modCheck = (req: Request, res: Response, next: NextFunction) => {
+  // If in testing mode, get user from req.body
+  if (process.env.NODE_ENV === "test") {
+    req.user = req.body.user;
+  }
+
   const user = req.user as IUser;
   if (!user || !user.moderator) {
     res.status(401).send("You are not a moderator");
@@ -22,6 +32,11 @@ export const modCheck = (req: Request, res: Response, next: NextFunction) => {
 
 // check if user is a verified Brown University community member
 export const brownCheck = (req: Request, res: Response, next: NextFunction) => {
+  // If in testing mode, get user from req.body
+  if (process.env.NODE_ENV === "test") {
+    req.user = req.body.user;
+  }
+
   const user = req.user as IUser;
   if (!user || !user.verifiedBrown) {
     res.status(401).send("You are not a verified Brown University member");
