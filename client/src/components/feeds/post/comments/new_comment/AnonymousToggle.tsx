066b2c389a553a55d8bb6cd17bb12cce0b04424d
nicholasvadasz <nicholas_vadasz@brown.edu>
2022-05-18 03:37:38 -0400

    Final iOS color fix

--- client/src/components/feeds/post/comments/new_comment/AnonymousToggle.tsx
+++ client/src/components/feeds/post/comments/new_comment/AnonymousToggle.tsx
@@ -25,7 +25,7 @@ function AnonymousToggle(props: AnonymousToggleProps) {
       {props.anonymous ? (
         <button className={className} onClick={props.anonymousToggle}>
           <AiOutlineEyeInvisible className="AnonymousIcon" />
-          <small className="PostingTag">
+          <small>
             Posting <strong>anonymously</strong>
           </small>
         </button>
@@ -39,7 +39,7 @@ function AnonymousToggle(props: AnonymousToggleProps) {
         >
           <AiOutlineEye className="AnonymousIcon" />
           <small className="AnonymousDetail">
-            <span className="PostingTag">
+            <span>
               Posting as <strong>{props.user?.name}</strong>
             </span>
           </small>
