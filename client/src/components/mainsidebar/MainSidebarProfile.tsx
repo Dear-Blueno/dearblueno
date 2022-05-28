e60ebc1f4856365aff93f940f62a05858c67e529
Dylan Hu <dylanhu7@gmail.com>
2022-05-28 12:56:03 -0400

    feat: profile footer links to profile page

--- client/src/components/mainsidebar/MainSidebarProfile.tsx
+++ client/src/components/mainsidebar/MainSidebarProfile.tsx
@@ -2,6 +2,7 @@ import LevelAndXPBar from "components/user/LevelAndXPBar";
 import XPBar from "components/user/XPBar";
 import { loadAuth } from "gateways/AuthGateway";
 import Image from "next/image";
+import Link from "next/link";
 import { useQuery } from "react-query";
 import styles from "./MainSidebarProfile.module.scss";
 
@@ -20,26 +21,28 @@ export default function MainSidebarProfile(props: MainSidebarProfileProps) {
     })
   );
   return (
-    <div className={styles.MainSidebarProfile}>
-      {user && (
-        <>
-          <div className={styles.MainSidebarProfilePicture}>
-            <Image
-              src={user.profilePicture}
-              alt="Profile picture"
-              width={60}
-              height={60}
-            />
-          </div>
-
-          <div className={styles.MainSidebarProfileNameAndXP}>
-            <div className={styles.MainSidebarProfileName}>
-              {user && user.name}
+    <Link href="/profile">
+      <div className={styles.MainSidebarProfile}>
+        {user && (
+          <>
+            <div className={styles.MainSidebarProfilePicture}>
+              <Image
+                src={user.profilePicture}
+                alt="Profile picture"
+                width={60}
+                height={60}
+              />
             </div>
-            <LevelAndXPBar xp={user.xp} />
-          </div>
-        </>
-      )}
-    </div>
+
+            <div className={styles.MainSidebarProfileNameAndXP}>
+              <div className={styles.MainSidebarProfileName}>
+                {user && user.name}
+              </div>
+              <LevelAndXPBar xp={user.xp} />
+            </div>
+          </>
+        )}
+      </div>
+    </Link>
   );
 }
