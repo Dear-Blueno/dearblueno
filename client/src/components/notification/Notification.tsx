e4946c0bbc365bb654abf77eb612850d50dd9be8
nicholasvadasz <nicholas_vadasz@brown.edu>
2022-06-12 19:48:40 -0500

    feat: added timestamps to notifications

--- client/src/components/notification/Notification.tsx
+++ client/src/components/notification/Notification.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 import { INotification } from "types/IUser";
 import styles from "./Notification.module.scss";
+import RelativeDate from "components/post/RelativeDate";
 
 type NotificationProps = {
   notification: INotification;
@@ -20,26 +22,34 @@ export default function Notification(props: NotificationProps) {
     );
   }
   console.log(props.notification);
+  let notifDate = new Date(props.notification.timestamp).toISOString();
   const context =
     "Content Goes here eventually and so on and so on and so on and so on and so on and so on and so on and so on and so on and so on and so on and so on and so on and so on and so on and so on";
 
   return (
-    <div className={styles.Notification}>
-      <div className={styles.NotificationHeader}>
-        <div className={styles.NotificationImageBox}>
-          <Image
-            src={props.notification.content.profilePicture}
-            width={50}
-            height={50}
-            className={styles.NotificationImage}
-          />
+    <Link href={`/post/${props.notification.content.postNumber}`}>
+      <div className={styles.Notification}>
+        <div className={styles.NotificationHeader}>
+          <div className={styles.NotificationHeaderLeft}>
+            <div className={styles.NotificationImageBox}>
+              <Image
+                src={props.notification.content.profilePicture}
+                width={50}
+                height={50}
+                className={styles.NotificationImage}
+              />
+            </div>
+            {notifText}
+          </div>
+          {/* conver timestamp to iso string */}
+
+          <RelativeDate date={notifDate} />
         </div>
-        {notifText}
+        <p className={styles.NotificationContext}>
+          {context.substring(0, 160)}
+          {context.length > 160 ? "…" : ""}
+        </p>
       </div>
-      <p className={styles.NotificationContext}>
-        {context.substring(0, 160)}
-        {context.length > 160 ? "…" : ""}
-      </p>
-    </div>
+    </Link>
   );
 }
