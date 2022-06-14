cb5d0b450e260d324a657205c4efdc901b566ea1
nicholasvadasz <nicholas_vadasz@brown.edu>
2022-06-13 19:20:36 -0500

    feat: beginning work on masonry for event page

--- client/src/components/feeds/EventsFeed.tsx
+++ client/src/components/feeds/EventsFeed.tsx
@@ -1,18 +1,63 @@
 import useUser from "hooks/useUser";
 import EventCard from "components/event/EventCard";
+import Masonry from "react-masonry-css";
+import styles from "./EventsFeed.module.scss";
 
 export default function EventsFeed() {
   const { user } = useUser();
+  const breakpointColumnsObj = {
+    default: 2,
+    700: 1,
+  };
+
   return (
     <div>
-      <EventCard
-        image="https://www.brown.edu/Departments/Music/sites/orchestra/images/2022-04/2022-spring-flyer-2.png"
-        title="Brown Orchestra Concert"
-        description="Come watch the Brown Orchestra at Sayles! Performance includes Firebird, Price’s 3rd Symphony, and more!"
-        location="Sayles Hall"
-        date="May 1, 2020"
-        numberOfAttendees={10}
-      />
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className={styles.myMasonryGrid}
+        columnClassName={styles.myMasonryGridColumn}
+      >
+        <EventCard
+          image="https://www.brown.edu/Departments/Music/sites/orchestra/images/2022-04/2022-spring-flyer-2.png"
+          title="Brown Orchestra Concert"
+          description="Come watch the Brown Orchestra at Sayles! Performance includes Firebird, Price’s 3rd Symphony, and more!"
+          location="Sayles Hall"
+          date="May 1, 2020"
+          numberOfAttendees={10}
+        />
+        <EventCard
+          image="https://www.brown.edu/Departments/Music/sites/orchestra/images/2022-04/2022-spring-flyer-2.png"
+          title="Brown Orchestra Concert"
+          description="Come watch the Brown Orchestra at Sayles! Performance includes Firebird, Price’s 3rd Symphony, and more. We will also have kenji bunch and a whole lot of cool people!"
+          location="Sayles Hall"
+          date="May 1, 2020"
+          numberOfAttendees={10}
+        />
+        <EventCard
+          image="https://www.brown.edu/Departments/Music/sites/orchestra/images/2022-04/2022-spring-flyer-2.png"
+          title="Brown Orchestra Concert"
+          description="Come watch the Brown Orchestra at Sayles! Performance includes Firebird, Price’s 3rd Symphony, and more!"
+          location="Sayles Hall"
+          date="May 1, 2020"
+          numberOfAttendees={10}
+        />
+        <EventCard
+          image="https://www.brown.edu/Departments/Music/sites/orchestra/images/2022-04/2022-spring-flyer-2.png"
+          title="Brown Orchestra Concert"
+          description="Come watch the Brown Orchestra at Sayles! Performance includes Firebird, Price’s 3rd Symphony, and more!"
+          location="Sayles Hall"
+          date="May 1, 2020"
+          numberOfAttendees={10}
+        />
+        <EventCard
+          image="https://www.brown.edu/Departments/Music/sites/orchestra/images/2022-04/2022-spring-flyer-2.png"
+          title="Brown Orchestra Concert"
+          description="Come watch the Brown Orchestra at Sayles! Performance includes Firebird, Price’s 3rd Symphony, and more!"
+          location="Sayles Hall"
+          date="May 1, 2020"
+          numberOfAttendees={10}
+        />
+      </Masonry>
     </div>
   );
 }
