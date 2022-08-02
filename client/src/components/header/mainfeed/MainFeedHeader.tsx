17fa5207dbded8ee1bb1bc600df0f8e21e1dabd6
Dylan Hu <git@dylan.hu>
2022-08-02 17:26:24 -0400

    feat: add UI for sorting by top

--- client/src/components/header/mainfeed/MainFeedHeader.tsx
+++ client/src/components/header/mainfeed/MainFeedHeader.tsx
@@ -1,74 +1,173 @@
 import styles from "./MainFeedHeader.module.scss";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useRouter } from "next/router";
 
-enum FeedSort {
-  Hot,
-  Top,
-  New,
-}
+type SortType = "hot" | "topWeek" | "topMonth" | "new" | "topAlltime";
 
 export default function MainFeedHeader() {
   const hotRef = useRef<HTMLHeadingElement>(null);
   const topRef = useRef<HTMLHeadingElement>(null);
   const newRef = useRef<HTMLHeadingElement>(null);
   const underlineRef = useRef<HTMLSpanElement>(null);
-  const [active, setActive] = useState<FeedSort>(FeedSort.Hot);
+  const router = useRouter();
+  const [choosingTop, setChoosingTop] = useState<boolean>(false);
 
-  const handleSwitch = useCallback(() => {
-    const refs = [hotRef, topRef, newRef];
-    const selectedRef = refs[active];
-    if (underlineRef.current && selectedRef.current) {
-      if (active === FeedSort.New) {
-        underlineRef.current.style.left = `${
-          selectedRef.current.offsetLeft - 2
-        }px`;
-        underlineRef.current.style.width = `${
-          selectedRef.current.offsetWidth + 4
-        }px`;
-      } else {
-        underlineRef.current.style.left = `${
-          selectedRef.current.offsetLeft - 1
-        }px`;
-        underlineRef.current.style.width = `${
-          selectedRef.current.offsetWidth + 4
-        }px`;
+  const parseSortQueryParams = useCallback((): SortType => {
+    if (router.query.sort) {
+      const sortType = router.query.sort as string;
+      if (sortType === "top") {
+        if (router.query.of === "week") {
+          return "topWeek";
+        } else if (router.query.of === "month") {
+          return "topMonth";
+        } else if (router.query.of === "alltime") {
+          return "topAlltime";
+        }
+      } else if (sortType === "hot" || sortType === "new") {
+        return sortType;
       }
     }
-  }, [active]);
+    return "hot";
+  }, [router.query.sort, router.query.of]);
 
-  useEffect(handleSwitch, [handleSwitch]);
+  const [sort, setSort] = useState<SortType>(parseSortQueryParams());
 
   useEffect(() => {
-    window.addEventListener("resize", handleSwitch);
+    setSort(parseSortQueryParams());
+  }, [parseSortQueryParams]);
+
+  const underlinedRef = useMemo(() => {
+    if (sort === "hot") {
+      return hotRef;
+    } else if (
+      sort === "topMonth" ||
+      sort === "topWeek" ||
+      sort === "topAlltime"
+    ) {
+      return topRef;
+    } else {
+      return newRef;
+    }
+  }, [sort]);
+
+  useEffect(() => {
+    if (underlineRef.current) {
+      if (choosingTop) {
+        underlineRef.current.style.display = "none";
+      } else {
+        underlineRef.current.style.display = "block";
+      }
+    }
+  }, [choosingTop, underlineRef, underlinedRef, sort]);
+
+  const moveUnderline = useCallback(() => {
+    if (underlineRef.current && underlinedRef.current) {
+      let leftAdjust = 0;
+      const widthAdjust = 4;
+      switch (sort) {
+        case "hot":
+          leftAdjust = -1;
+          break;
+        case "topWeek":
+          leftAdjust = -1.5;
+          break;
+        case "topMonth":
+          leftAdjust = -2.5;
+          break;
+        case "topAlltime":
+          leftAdjust = -2;
+          break;
+        case "new":
+          leftAdjust = -2;
+          break;
+      }
+
+      underlineRef.current.style.left = `${
+        underlinedRef.current.offsetLeft + leftAdjust
+      }px`;
+      underlineRef.current.style.width = `${
+        underlinedRef.current.offsetWidth + widthAdjust
+      }px`;
+    }
+  }, [sort, underlineRef, underlinedRef]);
+
+  useEffect(moveUnderline, [moveUnderline]);
+
+  useEffect(() => {
+    window.addEventListener("resize", moveUnderline);
     return () => {
-      window.removeEventListener("resize", handleSwitch);
+      window.removeEventListener("resize", moveUnderline);
     };
-  }, [handleSwitch]);
+  }, [moveUnderline]);
+
+  let topRefText;
+  if (choosingTop) {
+    topRefText = "Month";
+  } else {
+    if (sort === "topWeek") {
+      topRefText = "Week";
+    } else if (sort === "topMonth") {
+      topRefText = "Month";
+    } else if (sort === "topAlltime") {
+      topRefText = "All time";
+    } else {
+      topRefText = "Top";
+    }
+  }
 
   return (
-    <div className={styles.MainFeedHeader}>
+    <div
+      className={styles.MainFeedHeader}
+      onMouseLeave={() => !sort.startsWith("top") && setChoosingTop(false)}
+    >
       <div
         className={styles.MainFeedHeaderOption}
-        onClick={() => setActive(FeedSort.Hot)}
+        onClick={() => {
+          if (choosingTop) {
+            setSort("topWeek");
+            setChoosingTop(false);
+            void router.push("/?sort=top&of=week");
+          } else {
+            setSort("hot");
+            void router.push("/");
+          }
+        }}
       >
         <h3 className={styles.MainFeedHeaderOptionText} ref={hotRef}>
-          Hot
+          {choosingTop ? "Week" : "Hot"}
         </h3>
       </div>
       <div
         className={styles.MainFeedHeaderOption}
-        onClick={() => setActive(FeedSort.Top)}
+        onClick={() => {
+          if (!choosingTop) {
+            setChoosingTop(true);
+          } else {
+            setSort("topMonth");
+            setChoosingTop(false);
+            void router.push("/?sort=top&of=month");
+          }
+        }}
       >
         <h3 className={styles.MainFeedHeaderOptionText} ref={topRef}>
-          Top
+          {topRefText}
         </h3>
       </div>
       <div
         className={styles.MainFeedHeaderOption}
-        onClick={() => setActive(FeedSort.New)}
+        onClick={() => {
+          if (choosingTop) {
+            setSort("topAlltime");
+            setChoosingTop(false);
+            void router.push("/?sort=top&of=alltime");
+          } else {
+            setSort("new");
+            void router.push("/?sort=new");
+          }
+        }}
       >
         <h3 className={styles.MainFeedHeaderOptionText} ref={newRef}>
-          New
+          {choosingTop ? "All time" : "New"}
         </h3>
       </div>
       <span className={styles.FeedSelectionUnderline} ref={underlineRef} />
