06e1143e1ab335e6833819db169e5f7da77e0410
Nicholas Bottone <nick@bottone.io>
2022-09-07 00:15:29 -0400

    Fix next-pwa config

--- client/next.config.js
+++ client/next.config.js
@@ -11,4 +11,9 @@ const nextConfig = {
   },
 };
 
-module.exports = require("next-pwa")(nextConfig);
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA(nextConfig);
