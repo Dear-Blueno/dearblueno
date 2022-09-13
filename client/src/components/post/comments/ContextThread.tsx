5f596a8f33d2242a1ec6e002d0115852ec28e724
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2022-09-12 23:23:05 -0400

    fix: possible fix to comment moderation reload

--- client/src/components/post/comments/ContextThread.tsx
+++ client/src/components/post/comments/ContextThread.tsx
@@ -2,8 +2,10 @@ import styles from "./ContextThread.module.scss";
 import IComment from "types/IComment";
 import { IThread } from "./CommentSection";
 import Thread from "./Thread";
-import ApproveOrDeny from "../moderator/ApproveOrDeny";
 import CommentContext from "./CommentContext";
+import { InfiniteData, useQueryClient } from "@tanstack/react-query";
+import { approveComment } from "gateways/PostGateway";
+import toast from "react-hot-toast";
 
 export interface ContextThreadProps {
   thread: IComment;
@@ -13,6 +15,34 @@ export interface ContextThreadProps {
 }
 
 function ContextThread(props: ContextThreadProps) {
+  const queryClient = useQueryClient();
+  const handleAction = async (action: boolean) => {
+    const response = await approveComment(
+      props.thread.postNumber,
+      props.thread.commentNumber,
+      action
+    );
+    if (response.success) {
+      queryClient.setQueryData(
+        ["moderatorcomments"],
+        (data: InfiniteData<IComment[]> | undefined) => {
+          const newData = data
+            ? (JSON.parse(JSON.stringify(data)) as InfiniteData<IComment[]>)
+            : undefined;
+          newData?.pages.forEach(
+            (_, index, array) =>
+              (array[index] = array[index].filter(
+                (comment) => comment._id !== props.thread._id
+              ))
+          );
+          return newData;
+        }
+      );
+    } else {
+      toast.error((response.message as unknown as { message: string }).message);
+    }
+  };
+
   return (
     <a
       className={styles.ContextThreadLink}
@@ -40,11 +70,23 @@ function ContextThread(props: ContextThreadProps) {
         )}
         {props.moderatorView && (
           <div className={styles.ApproveOrDenyContainer}>
-            <ApproveOrDeny
+            {/* <ApproveOrDeny
               type="comment"
               postNumber={props.thread.postNumber}
               commentNumber={props.thread.commentNumber}
-            />
+            /> */}
+            <button
+              className={styles.ApproveButton}
+              onClick={() => void handleAction(true)}
+            >
+              Approve
+            </button>
+            <button
+              className={styles.DenyButton}
+              onClick={() => void handleAction(false)}
+            >
+              Deny
+            </button>
           </div>
         )}
       </div>
