2c5b0b15db130c12ae50411b3ab4788321cdea51
Dylan Hu <git@dylan.hu>
2022-12-21 17:45:33 -0500

    Fix reactions not working on default feed (#44)

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,8 +3,7 @@ import MainFeedSidebar from "components/sidebar/mainfeed/MainFeedSidebar";
 import Head from "next/head";
 import MainLayout from "components/layout/MainLayout";
 import MainFeedHeader from "components/header/mainfeed/MainFeedHeader";
-import { GetStaticProps, NextPage } from "next";
-import { getPosts } from "gateways/PostGateway";
+import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
@@ -22,25 +21,25 @@ const HomePage: NextPage = () => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getPosts(1, "hot");
-  if (posts.success) {
-    return {
-      props: {
-        initialPosts: posts.payload,
-      },
-      // Next.js will attempt to re-generate the page:
-      // - When a request comes in
-      // - At most every 30 seconds
-      revalidate: 30,
-    };
-  }
-  return {
-    props: {
-      initialPosts: [],
-    },
-    revalidate: 5,
-  };
-};
+// export const getStaticProps: GetStaticProps = async () => {
+//   const posts = await getPosts(1, "hot");
+//   if (posts.success) {
+//     return {
+//       props: {
+//         initialPosts: posts.payload,
+//       },
+//       // Next.js will attempt to re-generate the page:
+//       // - When a request comes in
+//       // - At most every 30 seconds
+//       revalidate: 30,
+//     };
+//   }
+//   return {
+//     props: {
+//       initialPosts: [],
+//     },
+//     revalidate: 5,
+//   };
+// };
 
 export default HomePage;
