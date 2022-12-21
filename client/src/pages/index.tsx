eb630c2f99f6e3800180151b4a86f1fde3a9f5e1
Dylan Hu <git@dylan.hu>
2022-12-21 17:42:29 -0500

    remove unused imports

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,8 +3,7 @@ import MainFeedSidebar from "components/sidebar/mainfeed/MainFeedSidebar";
 import Head from "next/head";
 import MainLayout from "components/layout/MainLayout";
 import MainFeedHeader from "components/header/mainfeed/MainFeedHeader";
-import { GetStaticProps, NextPage } from "next";
-import { getPosts } from "gateways/PostGateway";
+import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
