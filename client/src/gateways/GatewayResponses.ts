74c5ac937467e4d83fe5a248a8be7e830a4d42b2
Nicholas Bottone <nick@bottone.io>
2023-04-05 00:46:20 -0400

    Fix error message on failure gateway responses to be string

--- client/src/gateways/GatewayResponses.ts
+++ client/src/gateways/GatewayResponses.ts
@@ -1,3 +1,5 @@
+import { AxiosError } from "axios";
+
 export type IResponse<T> = ISuccessfulResponse<T> | IFailureResponse;
 
 interface ISuccessfulResponse<T> {
@@ -12,7 +14,7 @@ interface IFailureResponse {
   success: false;
 }
 
-export function successfulResponse<T>(payload: T): IResponse<T> {
+export function successfulResponse<T>(payload: T): ISuccessfulResponse<T> {
   return {
     message: "OK",
     payload: payload,
@@ -20,10 +22,22 @@ export function successfulResponse<T>(payload: T): IResponse<T> {
   };
 }
 
-export function failureResponse<T>(message: string): IResponse<T> {
+export function failureResponse(message: string): IFailureResponse {
   return {
     message: message,
     payload: null,
     success: false,
   };
+}
+
+export function failureResponseFromError(error: unknown): IFailureResponse {
+  if (!(error instanceof AxiosError)) {
+    return failureResponse(error as string);
+  }
+  const axiosError = error as AxiosError;
+  let message = axiosError.message;
+  if (axiosError.response) {
+    message += ` (${axiosError.response.statusText})`;
+  }
+  return failureResponse(message);
 }
