f2c49c12feddbf1f97078a577a0f634e6db83770
Nicholas Bottone <nick@bottone.io>
2023-04-04 21:49:12 -0400

    Implement SSG on post pages

--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -3,21 +3,24 @@ import { getPost } from "../../gateways/PostGateway";
 import Post from "components/post/Post";
 import NotFoundPage from "pages/404";
 import MainLayout from "components/layout/MainLayout";
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
+import { IResponse, failureResponse } from "gateways/GatewayResponses";
 
-// interface PostPageProps {
-//   post?: IPost;
-// }
+interface PostPageProps {
+  staticPost?: IResponse<IPost>;
+}
 
-const PostPage: NextPage = () => {
+const PostPage: NextPage<PostPageProps> = ({ staticPost }) => {
   const router = useRouter();
   const { id } = router.query;
   const postNumber = id ? Number(id) : undefined;
-  const { data } = useQuery(["post", postNumber], () =>
-    postNumber ? getPost(postNumber) : undefined
+  const { data } = useQuery(
+    ["post", postNumber],
+    () => (postNumber ? getPost(postNumber) : undefined),
+    { initialData: staticPost, initialDataUpdatedAt: Date.now() - 60000 }
   );
 
   if (!postNumber) {
@@ -52,31 +55,27 @@ function PostPageMain({ post }: PostPageMainProps) {
   return <Post post={post} />;
 }
 
-// export const getStaticProps: GetStaticProps = async (context) => {
-//   const postNumber = Number(context.params?.id as string);
-//   const post = await getPost(postNumber);
-//   if (post.success) {
-//     return {
-//       props: {
-//         post: post.payload,
-//       },
-//       // Next.js will attempt to re-generate the page:
-//       // - When a request comes in
-//       // - At most every 30 seconds
-//       revalidate: 30,
-//     };
-//   }
-//   return {
-//     props: {
-//       post: null,
-//     },
-//     revalidate: 30,
-//   };
-// };
+export const getStaticProps: GetStaticProps = async (context) => {
+  const postNumber = Number(context.params?.id as string);
+  const post = await getPost(postNumber);
+  if (post.success) {
+    return {
+      props: {
+        post: post,
+      },
+      revalidate: 30,
+    };
+  }
+  return {
+    props: {
+      post: failureResponse("Post not found"),
+    },
+    revalidate: 30,
+  };
+};
 
-// export function getStaticPaths() {
-//   // Server-render and cache pages on the fly.
-//   return { fallback: "blocking", paths: [] };
-// }
+export function getStaticPaths() {
+  return { fallback: "blocking", paths: [] };
+}
 
 export default PostPage;
